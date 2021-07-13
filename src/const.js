1d62584f39739f32efd286e96198bbe0cd0fd6a6
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-07-13 19:04:13 +0300

    Added fonts, not-found-screen and global slyle

---
+++ src/const.js
@@ -0,0 +1,25 @@
+const AppRoute = {
+  ROOT: `/`,
+  CONVERTER: `/converter`,
+  INFO: `/info`,
+  LOAN: `/loan`,
+  CONTACTS: `/contacts`,
+  QUESTION: `/question`,
+  LOGIN: `/login`,
+};
+
+const APIRoute = {
+  ROOT: `/`,
+};
+
+// const Currency = {
+//   RUB: `RUB`,
+//   USD: `USD`,
+//   EUR: `EUR`,
+//   GBP: `GBP`,
+//   CNY: `CNY`,
+// };
+
+// const APIKey = `498eb836c87d82b00421`;
+
+export {AppRoute, APIRoute};
